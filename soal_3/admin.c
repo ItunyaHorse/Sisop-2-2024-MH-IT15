dec5c21192c34ccdafd20621dc6377d0f9e65420
Ken <145765887+ItunyaHorse@users.noreply.github.com>
2024-04-20 04:24:27 +0700

    Update admin.c

--- soal_3/admin.c
+++ soal_3/admin.c
@@ -9,6 +9,39 @@
 #include <string.h>
 #include <time.h>
 
+void daemoner(){
+    pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    FILE *pid_file = fopen("daemon.pid", "w");
+    fprintf(pid_file, "%d", pid);
+    fclose(pid_file);
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+}
+
+
 void checkadmin(char *username) {
     int sementara[2];
     pid_t pid;
@@ -28,55 +61,127 @@ void checkadmin(char *username) {
   
 }
 
-void mulai(char *nama){
-  char logfile[100];
-  FILE *file_ptr;
-  sprintf(logfile, "%s.log", nama);
-  file_ptr = fopen(logfile, "w");
- //ps -eo pid,lstart,cmd | awk /mken/\'{print \"[\"$3\"-\"$4 \"-2024\"  \"]-[\" $5 \"]-\" $1 \"-\" $7}\'"
-  while (1) {
-    int bentar[2];
+void mulai(char *nama, int status){
+
+char logfile[100];
+FILE *file_ptr;
+sprintf(logfile, "%s.log", nama);
+file_ptr = fopen(logfile, "a");
+  if (status == 0){
+    int normal[2];
     pid_t pid;
-    pipe(bentar);
+    pipe(normal);
     pid = fork();
-    if (pid == 0) { 
-        close(bentar[0]);
-        dup2(bentar[1], STDOUT_FILENO);
-        close(bentar[1]);
+    if (pid == 0) {
+        dup2(normal[1], STDOUT_FILENO);
+        close(normal[0]);
         execlp("ps", "ps", "-eo", "pid,lstart,cmd", NULL);
-    } else {
-        close(bentar[1]);
-        dup2(bentar[0], STDIN_FILENO);
-        close(bentar[0]);
-        execlp("awk", "awk", "{print \"[\"$3\"-\"$4 \"-2024\"  \"]-[\" $5 \"]-\" $1 \"-\" $7}", NULL);
     }
-  sleep(30);
+    else{
+      dup2(normal[0], STDIN_FILENO);
+      close(normal[1]);
+      dup2(fileno(file_ptr), STDOUT_FILENO);
+      fclose(file_ptr);
+      execlp("awk", "awk", "{print \"[\"$3\"-\"$4 \"-2024\"  \"]-[\" $5 \"]-\" $1 \"-\" $7 \"_BERHASIL\"}", NULL);
+    }
   }
+
+  else {
+
+ int ps_tail[2];
+    int tail_awk[2];
+    pid_t pid_ps, pid_tail, pid_awk;
+
+    pipe(ps_tail);
+
+    // FFork ke 1 buat dasar
+    pid_ps = fork();
+    if (pid_ps == 0) {  
+        dup2(ps_tail[1], STDOUT_FILENO);
+        close(ps_tail[0]);  
+        execlp("ps", "ps", "-eo", "pid,lstart,cmd", NULL);
+    }
+
+    pipe(tail_awk);
+
+    // Fork ke 2 buat pipe 2
+    pid_tail = fork();
+    if (pid_tail == 0) {  
+        dup2(ps_tail[0], STDIN_FILENO);
+        close(ps_tail[1]);  
+        close(tail_awk[0]); 
+        dup2(tail_awk[1], STDOUT_FILENO);
+        close(tail_awk[1]); 
+        execlp("tail", "tail", "-n", "1", NULL);
+    }
+
+    // Fork ke 3 buat pipe 3
+    pid_awk = fork();
+    if (pid_awk == 0) {  
+        close(ps_tail[0]); 
+        close(ps_tail[1]);  
+        close(tail_awk[1]); 
+        dup2(tail_awk[0], STDIN_FILENO);
+        dup2(fileno(file_ptr), STDOUT_FILENO);
+        fclose(file_ptr);
+        execlp("awk", "awk", "{print \"[\"$3\"-\"$4 \"-2024\"  \"]-[\" $5 \"]-\" $1 \"-\" $7 \"_GAGAL\"}", NULL);
+    }
+
+    close(ps_tail[0]);
+    close(ps_tail[1]);
+    close(tail_awk[0]);
+    close(tail_awk[1]);
+  }
+}
+
+void penggagal(char *teks){
+  FILE *file_ptr;
+  file_ptr = fopen(teks, "w");
+  fprintf(file_ptr, "1");
+  fclose(file_ptr);
+}
+
+void hilanggagal(char *file){
+  FILE *file_ptr;
+  file_ptr = fopen(file, "w");
+  fprintf(file_ptr, "0");
+  fclose(file_ptr);
+}
+
+void pembunuh(){
+    FILE *gudang = fopen("daemon.pid", "r");
+    pid_t pid;
+    fscanf(gudang, "%d", &pid);
+    fclose(gudang);
+    kill(pid, 9);
 }
 
 int main(int argc, char *argv[]) {
 
     char *username = argv[1];
     char *user = argv[2];
+    int status;
+    FILE *state_file = fopen("state.txt", "r");
+    fscanf(state_file, "%d", &status);
 
-    if (strcmp(argv[1], "-m") == 0) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            mulai(user);
-            exit(0);
-        }
+    if (strcmp(argv[1], "-c") == 0) {
+      penggagal("state.txt");
     } 
-    else if (strcmp(argv[1], "-c") == 0) {
-        system("echo -c");
+    else if (strcmp(argv[1], "-m") == 0) {
+      daemoner();
+      while(1){
+        mulai(user, status);
+        sleep(60);
+      }
     } 
     else if (strcmp(argv[1], "-a") == 0) {
-        system("echo -a");
+      hilanggagal("state.txt");
+
     } 
     else if (strcmp(argv[1], "-s") == 0) {
-        system("echo -s");
+      pembunuh();
     } 
     else {
         checkadmin(username);
-        return 0;
     }
 }
